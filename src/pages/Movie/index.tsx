efaa9e31185884a4dea8c38f5bef70a05c8c9322
Paulo Matos <paulomatos4554@gmail.com>
2023-03-07 22:18:22 -0300

    inject dependencies for fetch api

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import MyCard from "../../components/Card";
+import api from "../../services/api";
 
 export default function Movie() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const data = await api.get("/movie?title=skyfall&apikey=8384263e");
+        setMovie(data);
+        console.log("dados:", data);
+      } catch {
+        // continue
+      }
+    }
+    if (Object.keys(movie).length === 0) {
+      fetchMovie();
+    }
+  }, [movie]);
+
   return (
-    <div className="App">
+    <div>
       <MyCard title="Meu título" text="Meu texto" hasButton={false} />
     </div>
   );
