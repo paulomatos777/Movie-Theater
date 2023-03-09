603133ea15649b2b3701c8457e912e7ff2e64618
Paulo Matos <paulomatos4554@gmail.com>
2023-03-09 09:03:15 -0300

    update variables

--- src/movie.controller.ts
+++ src/movie.controller.ts
@@ -5,9 +5,12 @@ import axios from 'axios';
 export class MovieController {
   @Get()
   async getMovie(@Query('title') title: string, @Query('year') year: string) {
-    const url = `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=8384263e`;
+    const apiKey = '8384263e';
+
+    const url = `http://www.omdbapi.com/?t=${title}&y=${year}&apikey=${apiKey}`;
+
     const response = await axios.get(url);
-    console.log(response.data);
+
     return response.data;
   }
 }
